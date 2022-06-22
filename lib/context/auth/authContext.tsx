b311f499165dd13adece4966ed35338d291ba109
= <=>
2022-06-22 12:40:31 -0700

    add signin and signout functionality

--- lib/context/auth/authContext.tsx
+++ lib/context/auth/authContext.tsx
@@ -1,6 +1,19 @@
-import { ApiError, Provider, Session, User } from "@supabase/supabase-js";
-import { createContext, FunctionComponent, ReactNode, useState } from "react";
+import {
+  ApiError,
+  AuthChangeEvent,
+  Provider,
+  Session,
+  User,
+} from "@supabase/supabase-js";
+import {
+  createContext,
+  FunctionComponent,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
 import { supabase } from "@lib/supabase";
+import { isNullOrUndefined } from "util";
 
 type SignInOptions = {
   email?: string;
@@ -37,6 +50,34 @@ type Props = {
 export const AuthProvider: FunctionComponent = ({ children }: Props) => {
   const [session, setSession] = useState<Session | null>(null);
   const [user, setUser] = useState<User | null>(null);
+
+  const setServerSession = async (
+    event: AuthChangeEvent,
+    session: Session | null
+  ) => {
+    await fetch("/api/auth", {
+      method: "POST",
+      headers: new Headers({ "Content-type": "application/json" }),
+      credentials: "same-origin",
+      body: JSON.stringify({ event, session }),
+    });
+  };
+
+  useEffect(() => {
+    const session = supabase.auth.session();
+    setSession(session);
+    setUser(session?.user ?? null);
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        await setServerSession(event, session);
+        setUser(session?.user ?? null);
+      }
+    );
+    
+    /* cleanup function */
+    return () => authListener?.unsubscribe();
+  }, []);
 
   const values = {
     session,
---
+++ pages/api/auth.ts
@@ -0,0 +1,9 @@
+import { supabase } from '@lib/supabase'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+ supabase.auth.api.setAuthCookie(req, res)
+}
